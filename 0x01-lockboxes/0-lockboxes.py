36656a8ead2dd452fc27a1f248de9e4f08870f09
Mboko David <mbokodavid@gmail.com>
2023-02-02 20:13:00 +0100

    LoadBox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Lockboxes Contains method that finds the keys to
+open other lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Function that determines if you can open all the lockboxes
+    Args:
+        boxes: list of lists of integers
+    Returns:
+        True if you can open all the lockboxes, False otherwise
+    """
+    unlocked = set()
+
+    for box_id, box in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            unlocked.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                unlocked.add(key)
+        if len(unlocked) == len(boxes):
+            return True
+    return False
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,5 @@
+# Lockboxes
+
+This is test used as part of programming to teach various concepts.
+
+In this example, we will try to find the keys that can open different lockboxes.
