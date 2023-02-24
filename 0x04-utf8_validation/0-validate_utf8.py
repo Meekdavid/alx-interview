0e01ca8a76ff760843e810138bf18ffce557c26d
David Mboko <mbokodavid@gmail.com>
2023-02-24 06:16:28 +0100

    Retesting UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
-"""
-Validate UTF-8 encoded text.
-
-This is a simple example of how to use the utf8_validation module.
-"""
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
     """
-    Check if data os a valid UTF-8 encoding
-
-    Args:
-        data: A list of bytes.
-
-    Returns:
-        True if data is a valid UTF-8 encoding, False otherwise.
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    for byte in data:
-        if byte >> 7 == 0:
-            continue
-        elif byte >> 5 == 0b110:
-            continue
-        elif byte >> 4 == 0b1110:
-            continue
-        elif byte >> 3 == 0b11110:
-            continue
-        else:
-            return False
-    return True
+    number_bytes = 0
 
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
